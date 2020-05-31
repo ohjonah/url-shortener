5996f6b287e5e3b2f79f6262f3f3fe608a16f89e
ohjonah <ohjonah@gmail.com>
2020-05-31 14:30:28 -0700

    Adds URL Shortener Service

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
+
+const app = express(cors());
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  console.log('shortUrls:', shortUrls);
+  res.render('index', { shortUrls: shortUrls });
+});
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect('/');
+});
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (!shortUrl) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+  res.redirect(shortUrl.full);
+});
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Listening on port ${process.envPORT || 3000}`)
+);
